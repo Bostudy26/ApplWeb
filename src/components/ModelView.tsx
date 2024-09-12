8cbd72444f4aca6d75b3d64a1b911e9a6c1139c9
Bo <boprojectbo@gmail.com>
2024-09-12 13:34:29 +0200

    Feat: Modelview + model

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { PerspectiveCamera, View } from "@react-three/drei"
 
-const ModelView = () => {
+interface ModelViewProps {
+    index: number
+    groupRef: any
+    gsapType: string
+    controlRef: any
+    setRationState: any
+    item: any
+    size: string
+}
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRationState, item, size} : ModelViewProps) => {
   return (
-    <div>ModelView</div>
+    <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full
+                ${index === 2} ? 'right-[-100%] : ''
+            `}
+    >
+        {/* Ambient Light */}
+        <ambientLight 
+            intensity={0.3}
+        />
+
+        <PerspectiveCamera
+            makeDefault
+            position={[0,0,4]}
+        />
+    </View>
   )
 }
 
