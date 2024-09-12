d9390fbf485bb55d552f1f7e613a36d83cac70bf
Bo <boprojectbo@gmail.com>
2024-09-12 17:14:58 +0200

    Feat:Features

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
-import Highlights from './sections/Highlights'
-import Model from './sections/Model'
+// import Navbar from './sections/Navbar'
+// import Hero from './sections/Hero'
+// import Highlights from './sections/Highlights'
+// import Model from './sections/Model'
+
+import Features from "./sections/Features"
 
 const App = () => {
   return (
     <main className='bg-black'>
-      <Navbar/>
+      {/* <Navbar/>
       <Hero/>
       <Highlights/>
-      <Model/>
+      <Model/> */}
+      <Features/>
     </main>
   )
 }
